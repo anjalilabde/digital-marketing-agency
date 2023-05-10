a7ac5977bc6df382cbc5bb4d048d2e856d8fc8dc
anjalilabde <82899008+anjalilabde@users.noreply.github.com>
2023-05-10 16:27:48 +0530

    UPDATE

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          "primary": "#162466",
+          "dark": "#181B31",
+        },
+        gray: {
+          "light": "#D6D6E3",
+          "dark": "#555555",
+        },
+        red: {
+          "dark": "#FF3B33",
+          "light": "#FFD1C8",
+        }
+      },
+    },
+    fontFamily: {
+      nunito: ["Nunito, Noto Sans, sans-serif"],
+      noto: ["Noto Sans, sans-serif"],
+    },
+    backgroundImage: {
+      "bannerimage": "url('/images/Background.png')",
+      "cardbg": "url('/images/card-bg-image.png')",
+      "processbg": "url('/images/process-bg.png')",
+      "testimonialbg": "url('/images/testimonial-bg.png')",
+      "subscribebg": "url('/images/Subscribe-bg-img.png')",
+      "aboutus-bg": "url('/images/aboutusbg-img.png')",
+      "service-cta-bg": "url('/images/service-cta.png')",
+      "service-banner-bg": "url('/images/services-bg.png')",
+      "contactus-banner-bg": "url('/images/service-cta.png')",
+      "terms-banner-bg": "url('/images/tc-bg.png')",
+      "terms-content-bg": "url('/images/terms-bg.png')",
+      "career-banner-bg": "url('/images/service-cta.png')",
+      "blog-card-bg": "url('/images/Blog-bg.png')",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        DEFAULT: '1320px'
+      },
+    },
+
+  },
+  plugins: [],
+}
+
